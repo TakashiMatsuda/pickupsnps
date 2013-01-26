d4852efde07795726095fa0eb0f61ec6f07d9ab3
Takashi Matsuda <mazda.takasky@gmail.com>
2013-01-26 23:56:58 +0900

    テストコードを追加。これから書きます。

---
+++ src/snp/SNPTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package snp;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author takashi
+ *
+ */
+public class SNPTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}

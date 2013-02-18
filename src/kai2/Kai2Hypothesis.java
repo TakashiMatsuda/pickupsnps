c54fea1e090167c5125cc300ece4f94e7b008796
Takashi Matsuda <mazda.takasky@gmail.com>
2013-02-18 13:28:59 +0900

    単純同期

--- src/kai2/Kai2Hypothesis.java
+++ src/kai2/Kai2Hypothesis.java
@@ -13,14 +13,14 @@ import eco.Eco;
 import eco.EcoList;
 
 /**
- * メインクラス
- * 動作の流れをここに書いてください。
+ * メインクラス 動作の流れをここに書いてください。
+ * 
  * @author takashi
  * 
  */
 public class Kai2Hypothesis {
 	public static boolean SIGN = true;
-	
+
 	private SNPList snplist;
 	private EcoList ecolist;
 	private PhenoList phenolist;
@@ -30,7 +30,7 @@ public class Kai2Hypothesis {
 	 * 
 	 */
 	Kai2Hypothesis() {
-		this.snplist = new SNPList(/*216130*/);// こういうコンストラクタを作りたい
+		this.snplist = new SNPList(/* 216130 */);// こういうコンストラクタを作りたい
 		this.ecolist = new EcoList();
 		this.phenolist = new PhenoList();
 
@@ -46,8 +46,7 @@ public class Kai2Hypothesis {
 		singleton.run(SIGN);
 
 	}
-	
-	
+
 	/**
 	 * このクラスの初期化手続き
 	 */
@@ -65,63 +64,59 @@ public class Kai2Hypothesis {
 		loadphenofile();
 		System.out.println("読み込みを完了しました");
 	}
-	
-	
+
 	/*
 	 * pheno.txtを読み込んで、ecolistを作成します。(実装完了)
 	 */
 	private void loadecofile() {
-		try{
+		try {
 			String line = null;
 			BufferedReader br = new BufferedReader(new FileReader("pheno.txt"));
 			StringBuilder a;
 			double[] phenovalues = new double[22];
 			String[] elements = new String[5];
 			String[] prePV = new String[22];
-			while((line = br.readLine()) != null){
+			while ((line = br.readLine()) != null) {
 				/*
 				 * 新しいEcoインスタンスを作成し、EcoListに追加します。
 				 */
 				elements = line.split("	");
-				
-//				もし必要であればelements[0] ~ [3]も利用してください。今は捨てています。
+
+				// もし必要であればelements[0] ~ [3]も利用してください。今は捨てています。
 				prePV = elements[4].split(",");
-				for(int i = 0; i < prePV.length; i++){
-					if (!prePV[i].equals("NA")){
+				for (int i = 0; i < prePV.length; i++) {
+					if (!prePV[i].equals("NA")) {
 						phenovalues[i] = Double.parseDouble(prePV[i]);
-					}
-					else{
-						phenovalues[i] = -1;//FIXME この点を考慮したコードを下流実装に注入する必要がある。
+					} else {
+						phenovalues[i] = -1;// FIXME この点を考慮したコードを下流実装に注入する必要がある。
 					}
 				}
 				Eco eco = new Eco(phenovalues);
 				ecolist.add(eco);
-				
+
 			}
 			br.close();
-		} catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
-	
 	/*
 	 * pheno_info.txtを読み込み、phenolistを作成します。
-	 * 実は特に今は必要ない。最終的にどのphenoの情報を出力するときに必要になる。
-	 * (未実装)
+	 * 実は特に今は必要ない。最終的にどのphenoの情報を出力するときに必要になる。 (未実装)
 	 */
 	private void loadphenofile() {
-		try{
+		try {
 			String line = null;
-			BufferedReader br = new BufferedReader(new FileReader("pheno_info.txt"));
-			
-			
+			BufferedReader br = new BufferedReader(new FileReader(
+					"pheno_info.txt"));
+
 			br.close();
-		} catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	/**
@@ -131,7 +126,7 @@ public class Kai2Hypothesis {
 		try {
 			String line = null;
 			BufferedReader br = new BufferedReader(new FileReader("snps.txt"));
-//			StringBuilder chrom = new StringBuilder();
+			// StringBuilder chrom = new StringBuilder();
 			int pos = 0;
 			String[] elements = new String[3];
 			char[] alleles = new char[199];
@@ -149,11 +144,10 @@ public class Kai2Hypothesis {
 				// やろうと思えば、preallelesの生成はいらないはずです。(字数を数えていけばよい)
 				prealleles = elements[2].split(",");
 				for (int i = 0; i < 199; i++) {
-						alleles[i] = prealleles[i].charAt(0);
+					alleles[i] = prealleles[i].charAt(0);
 				}
 				SNP snp = new SNP(elements[0], pos, alleles);
 				snplist.add(snp);
-				
 
 			}
 			br.close();
@@ -164,8 +158,7 @@ public class Kai2Hypothesis {
 	}
 
 	/**
-	 * 実行関数。重要。
-	 * p値の表を出力し、QQプロットのためのtsvファイルを出力し、多重検定の結果上位のSNPsリストを出力します。
+	 * 実行関数。重要。 p値の表を出力し、QQプロットのためのtsvファイルを出力し、多重検定の結果上位のSNPsリストを出力します。
 	 * 
 	 * @param trigger
 	 */
@@ -179,29 +172,27 @@ public class Kai2Hypothesis {
 		singleton.calcHighRankSNPs(snplist);
 		singleton.writeHighRankSNPs(snplist);
 	}
-	
-	
+
 	/**
 	 * 
 	 * @param snplist2
 	 */
 	private void calcHighRankSNPs(SNPList snplist2) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 	}
 
 	/**
 	 * QQプロットの作成段階です。
+	 * 
 	 * @param snplist2
 	 */
 	private Set calcQQ(SNPList snplist2) {
 		// FIXME 次はここから
-//		各SNPについてqqプロットを作成する。
-		
-		
-		
+		// 各SNPについてqqプロットを作成する。
+
 		return null;
-		
+
 	}
 
 	/**
@@ -224,7 +215,7 @@ public class Kai2Hypothesis {
 	 * @param snplist
 	 */
 	private void writeHighRankSNPs(SNPList snplist) {
-		
+
 	}
 
 	/**
@@ -237,7 +228,7 @@ public class Kai2Hypothesis {
 		System.out.println("p値ベクトルを計算しています。");
 		snplist.setAllphenotablelist(ecolist);
 		snplist.setAllPValues();
-		
+
 		System.out.println("p値ベクトルの計算が終了しました。");
 	}
 
